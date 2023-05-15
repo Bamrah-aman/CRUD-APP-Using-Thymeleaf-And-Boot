1ec207345ed0bdceb237e33046818fb73e2ace11
GoBrilliant-Amandeep <GoBrilliant-Amandeep@DESKTOP-E1VQIJQ.mshome.net>
2023-05-15 10:23:05 +0530

    Added statment to main app

--- src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -8,6 +8,7 @@ public class ThymeleafdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
+		System.out.println("What is happening");
 	}
 
 }
